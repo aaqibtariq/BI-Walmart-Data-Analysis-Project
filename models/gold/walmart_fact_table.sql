e1d60ec9705e2a0e25848253ecf540575975632c
aaqib tariq <tradonews@gmail.com>
2026-01-15 03:39:27 +0000

    updated snapshot sql and walmart_fact_table as it was missing some columns and updated gold_schema.yml

--- models/gold/walmart_fact_table.sql
+++ models/gold/walmart_fact_table.sql
@@ -9,24 +9,27 @@ with src as (
 
   select
     store_id,
-    store_date,
-    temperature,
+    dept_id,
+
+    -- Date_id FK
+    to_number(to_char(store_date::date, 'YYYYMMDD')) as date_id,
+
+    store_size,
+    weekly_sales as store_weekly_sales,
+
     fuel_price,
+    temperature,
+    unemployment,
+    cpi,
     markdown1,
     markdown2,
     markdown3,
     markdown4,
     markdown5,
-    cpi,
-    unemployment,
-    is_holiday,
 
-    dbt_valid_from as effective_from,
-    dbt_valid_to   as effective_to,
-    case
-      when dbt_valid_to is null then true
-      else false
-    end as is_current,
+    -- SCD2 versioning fields
+    dbt_valid_from as vrsn_start_date,
+    dbt_valid_to   as vrsn_end_date,
 
     current_timestamp() as insert_date,
     current_timestamp() as update_date
@@ -34,7 +37,7 @@ with src as (
   from WALMART_DB.SNAPSHOTS.walmart_fact_snapshot
 
   {% if is_incremental() %}
-    where dbt_valid_from > (select max(effective_from) from {{ this }})
+    where dbt_valid_from > (select max(vrsn_start_date) from {{ this }})
   {% endif %}
 
 )
--- snapshots/walmart_fact_snapshot.sql
+++ snapshots/walmart_fact_snapshot.sql
@@ -4,10 +4,13 @@
   config(
     target_database='WALMART_DB',
     target_schema='SNAPSHOTS',
-    unique_key="store_id || '-' || to_varchar(store_date)",
+
+    unique_key="store_id || '-' || dept_id || '-' || to_varchar(store_date)",
 
     strategy='check',
     check_cols=[
+      'weekly_sales',
+      'store_size',
       'temperature',
       'fuel_price',
       'markdown1',
@@ -22,19 +25,60 @@
   )
 }}
 
+with dept as (
+  select
+    store_id,
+    dept_id,
+    store_date,
+    weekly_sales,
+    is_holiday
+  from {{ ref('stg_department') }}
+),
+
+facts as (
+  select
+    store_id,
+    store_date,
+    temperature,
+    fuel_price,
+    markdown1,
+    markdown2,
+    markdown3,
+    markdown4,
+    markdown5,
+    cpi,
+    unemployment
+  from {{ ref('stg_facts') }}
+),
+
+stores as (
+  select
+    store_id,
+    store_size
+  from {{ ref('stg_stores') }}
+)
+
 select
-  store_id,
-  store_date,
-  temperature,
-  fuel_price,
-  markdown1,
-  markdown2,
-  markdown3,
-  markdown4,
-  markdown5,
-  cpi,
-  unemployment,
-  is_holiday
-from {{ ref('stg_facts') }}
+  d.store_id,
+  d.dept_id,
+  d.store_date,
+  s.store_size,
+  d.weekly_sales,
+  f.temperature,
+  f.fuel_price,
+  f.markdown1,
+  f.markdown2,
+  f.markdown3,
+  f.markdown4,
+  f.markdown5,
+  f.cpi,
+  f.unemployment,
+  d.is_holiday
+from dept d
+left join facts f
+  on d.store_id = f.store_id
+ and d.store_date = f.store_date
+left join stores s
+  on d.store_id = s.store_id
 
 {% endsnapshot %}
